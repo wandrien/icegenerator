wandrien/icegenerator

commit d9870026f957a8b35db139e9ab092b0096c3ef52

Add method cConfig::GetIntValue()

src/selector.cpp
@@ -59,7 +59,7 @@ void *selector(void *arg)
 
   while (!ok_to_end)
   {
-    i = (((conf_obj->GetValue(CONFIG_SHUFFLE) != NULL) && (!atoi(conf_obj->GetValue(CONFIG_SHUFFLE))))? 1 : (rand() % 256));
+    i = (conf_obj->GetIntValue(CONFIG_SHUFFLE, 0)) ? 1 : (rand() % 256);
 
     sem_obj->Wait(data_mutex);
 
@@ -88,7 +88,7 @@ void *selector(void *arg)
     }
     else if (ready_obj->Empty())
     {
-      if ((conf_obj->GetValue(CONFIG_LOOP) == NULL) || atoi(conf_obj->GetValue(CONFIG_LOOP)))
+      if (conf_obj->GetIntValue(CONFIG_LOOP, 0))
       {
         cCircularList *temp;
 

src/circular.cpp
@@ -640,8 +640,13 @@ void cCircularList::ParsePlayList(cConfig *object)
   if (!strcmp(parsed_strs[0],"PTH"))
   {
     for (i = 1; i < strs_ind; i++)
-      if (stat(parsed_strs[i],&temp_stat) != -1)
-        FillWithPath(parsed_strs[i],((atoi(object->GetValue(CONFIG_FORMAT)))? _mp3 : _ogg),((object->GetValue(CONFIG_RECURSIVE) != NULL) && atoi(object->GetValue(CONFIG_RECURSIVE))));
+      if (stat(parsed_strs[i],&temp_stat) != -1) {
+        FillWithPath(
+          parsed_strs[i],
+          object->GetIntValue(CONFIG_FORMAT, 0) ? _mp3 : _ogg,
+          object->GetIntValue(CONFIG_RECURSIVE, 0)
+        );
+      }
   }
   else if (!strcmp(parsed_strs[0],"SQL"))
   {

src/config.cpp
@@ -55,6 +55,7 @@
 	
 #endif
 
+#include <stdlib.h>
 #include "config.h"
 
 static const char * keys[MAX_KEYS] = {
@@ -145,6 +146,14 @@ void cConfig::SetValue(config_key_type key, const char *value)
 char * cConfig::GetValue(config_key_type key) const
 {
   return Table[key];
+}
+
+int cConfig::GetIntValue(config_key_type key, int default_value) const
+{
+  char * value = GetValue(key);
+  if (value)
+    return atoi(value);
+  return default_value;
 }
 
 int cConfig::ParseLine(const char *buf, char *key, char *value)

src/config.h
@@ -63,6 +63,7 @@ public:
   cConfig(const char *path);
   ~cConfig();
   char *GetValue(config_key_type key) const;
+  int GetIntValue(config_key_type key, int default_value) const;
   void SetValue(config_key_type key, const char *value);
 };
 

src/data_server.cpp
@@ -89,7 +89,7 @@ void *data_server(void *arg)
   {
     memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
-    sin.sin_port = htons(atoi(conf_obj->GetValue(CONFIG_DATAPORT)));
+    sin.sin_port = htons(conf_obj->GetIntValue(CONFIG_DATAPORT, 0));
     if (!sin.sin_port)
       sin.sin_port = 8796;
     sin.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -208,7 +208,7 @@ void close_data_server()
 
   memset(&sin, 0, sizeof(sin));
   sin.sin_family = AF_INET;
-  sin.sin_port = htons(atoi(conf_obj->GetValue(CONFIG_DATAPORT)));
+  sin.sin_port = htons(conf_obj->GetIntValue(CONFIG_DATAPORT, 0));
   if (!sin.sin_port)
     sin.sin_port = 8796;
   sin.sin_addr.s_addr = inet_addr("127.0.0.1");

src/data_streamer.cpp
@@ -54,7 +54,7 @@ void *data_streamer(void *arg)
 {
   sig_obj->Block();
   
-  if ((conf_obj->GetValue(CONFIG_METAUPDATE) == NULL) || (atoi(conf_obj->GetValue(CONFIG_METAUPDATE)) > 0))
+  if (conf_obj->GetIntValue(CONFIG_METAUPDATE, 5) > 0)
   {
     cMetaDataList *global = NULL, *directory = NULL, *file = NULL, *p = NULL;
     bool loc_changed = false;
@@ -157,7 +157,7 @@ void *data_streamer(void *arg)
 
       if (strlen(temp_buf) > 0)
         stream_obj->UpdateMetaData(temp_buf);
-      sleep(((conf_obj->GetValue(CONFIG_METAUPDATE) != NULL)? atoi(conf_obj->GetValue(CONFIG_METAUPDATE)) : 5));
+      sleep(conf_obj->GetIntValue(CONFIG_METAUPDATE, 5));
     }
 
     

src/log.cpp
@@ -75,7 +75,7 @@ cLog::cLog(cConfig *object)
 {
   int_buf = new char[INTERNAL_BUF_SIZE];
 
-  switch (LogSelected = ((log_type) atoi(object->GetValue(CONFIG_LOG))))
+  switch (LogSelected = ((log_type) object->GetIntValue(CONFIG_LOG, 0)))
   {
     case _none: break;
     case _system: openlog("Icecast Generator",LOG_PID,LOG_USER);

src/streamer.cpp
@@ -118,7 +118,7 @@ void cStreamer::SetData(cConfig *object)
           shout_set_host(ShoutData,object->GetValue(CONFIG_IP));
           break;
         case CONFIG_PORT:
-          shout_set_port(ShoutData,atoi(object->GetValue(CONFIG_PORT)));
+          shout_set_port(ShoutData,object->GetIntValue(CONFIG_PORT, 0));
           break;
         case CONFIG_MOUNT:
           shout_set_mount(ShoutData,object->GetValue(CONFIG_MOUNT));
@@ -127,7 +127,7 @@ void cStreamer::SetData(cConfig *object)
           shout_set_password(ShoutData,object->GetValue(CONFIG_PASSWORD));
           break;
         case CONFIG_SERVER:
-          switch (atoi(object->GetValue(CONFIG_SERVER)))
+          switch (object->GetIntValue(CONFIG_SERVER, 0))
           {
             case 2:
               shout_set_protocol(ShoutData,SHOUT_PROTOCOL_HTTP);
@@ -157,13 +157,13 @@ void cStreamer::SetData(cConfig *object)
           shout_set_audio_info(ShoutData,SHOUT_AI_BITRATE,object->GetValue(CONFIG_BITRATE));
           break;
         case CONFIG_PUBLIC:
-          shout_set_public(ShoutData,atoi(object->GetValue(CONFIG_PUBLIC)));
+          shout_set_public(ShoutData,object->GetIntValue(CONFIG_PUBLIC, 0));
           break;
         case CONFIG_DUMPFILE:
           shout_set_dumpfile(ShoutData,object->GetValue(CONFIG_DUMPFILE));
           break;
         case CONFIG_FORMAT:
-          switch (atoi(object->GetValue(CONFIG_FORMAT)))
+          switch (object->GetIntValue(CONFIG_FORMAT, 0))
           {
             case 0:
               shout_set_format(ShoutData,SHOUT_FORMAT_VORBIS);

commit a9b9cafaf5fa7f170a6ec6031ab618ca637fab23

Fix a bug introduced in d9870026f957a8b35db139e9ab092b0096c3ef52

src/selector.cpp
@@ -59,7 +59,7 @@ void *selector(void *arg)
 
   while (!ok_to_end)
   {
-    i = (conf_obj->GetIntValue(CONFIG_SHUFFLE, 0)) ? 1 : (rand() % 256);
+    i = (conf_obj->GetIntValue(CONFIG_SHUFFLE, 0)) ? (rand() % 256) : 1;
 
     sem_obj->Wait(data_mutex);
 